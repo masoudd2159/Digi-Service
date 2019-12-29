ba9fe9b3172f586134a8021ca62b5ca2d17bf6ed
Masoud <masoudd2159@gmail.com>
2019-12-29 21:50:19 +0330

    added icon to project

---
+++ app/src/main/java/ir/ac/sku/service/digiservice/activity/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package ir.ac.sku.service.digiservice.activity;
+
+import android.os.Bundle;
+
+import ir.ac.sku.service.digiservice.R;
+import ir.ac.sku.service.digiservice.util.MyActivity;
+
+public class SplashScreenActivity extends MyActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
